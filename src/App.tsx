63d9186d8258d20816a85fc375ce75479cbe1836
DatsOlena <datsolena@gmail.com>
2025-07-02 20:55:19 +0200

    Remove duplication

--- src/App.tsx
+++ src/App.tsx
@@ -93,7 +93,6 @@ export const App: React.FC = () => {
         )}
       </div>
 
-      <ul>
         <ul>
           {visibleGoods.map(good => (
             <li key={good} data-cy="Good">
@@ -101,7 +100,6 @@ export const App: React.FC = () => {
             </li>
           ))}
         </ul>
-      </ul>
     </div>
   );
 };
